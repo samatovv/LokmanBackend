7c8204feaeecbb700b96aa3a04f50c1db277146a
Abdurahman <abdurahmansamatovic@gmail.com>
2025-06-05 23:58:58 +0600

    feat: CEO optimization

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,13 @@ import BannerSlider from "@/features/Banner";
 import ClientCategories from "@/features/Categories/ui/ClientCategories";
 import PopularCategories from "@/features/PopularCategories/ui";
 
+export const metadata = {
+  title: 'Каталог медицинского оборудования | SigmaMed',
+  description: 'Широкий выбор медицинского оборудования: тонометры, ингаляторы, стерилизаторы и др.',
+  keywords: 'медицинское оборудование, купить тонометр, стерилизаторы, ингаляторы, SigmaMed',
+};
+
+
 export default function Home() {
   return (
     <div>
